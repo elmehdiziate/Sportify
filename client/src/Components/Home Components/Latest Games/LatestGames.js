a7d3d2c9306249f2deaee569e3d34c98aee5f92e
Ayman Ait Achour <a.aitachour@aui.ma>
2023-12-13 03:32:40 +0100

    checking

--- client/src/Components/Home Components/Latest Games/LatestGames.js
+++ client/src/Components/Home Components/Latest Games/LatestGames.js
@@ -3,6 +3,7 @@ import './LatestGames.css';
 import pastGif from "../Assets/past.gif";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GameInformation from "../Game Information/GameInformation";
+import lazyIcon from '../Assets/lazy.gif';
 import axios from "axios";
 
 
@@ -22,20 +23,39 @@ export default function LatestGames(props){
     };
     fetchData();
 }, [userID]);
-    
-    return(
-    <>
-        <div className="upcoming-games scrollingDiv">
-            <div className="header">
-                <h5 className="title">Upcoming Game(s) :</h5>
-                <img className="upcoming-games-icon flip-horizontally" src={pastGif} alt="icon"/>
-            </div>
-            <hr className="new-line"/>
 
-            {filteredData.map((game) => {
-                return <GameInformation game={game} />
-            })}
-        </div>
-    </>
-    );
+    if(filteredData != 0){
+        return(
+            <>
+                <div className="upcoming-games scrollingDiv">
+                    <div className="header">
+                        <h5 className="title">Latest Game(s) :</h5>
+                        <img className="upcoming-games-icon flip-horizontally" src={pastGif} alt="icon"/>
+                    </div>
+                    <hr className="new-line"/>
+        
+                    {filteredData.map((game) => {
+                        return <GameInformation game={game} />
+                    })}
+                </div>
+            </>
+            );
+    }
+    else{
+        return<>
+
+                <div className="upcoming-games scrollingDiv">
+                    <div className="header">
+                        <h5 className="title">Latest Game(s) :</h5>
+                        <img className="upcoming-games-icon" src={pastGif} alt="icon"/>
+                    </div>
+                    <hr className="new-line"/>
+                    <div className="sleepIcon">
+                        <img src={lazyIcon} className="sleep"/>
+                    </div>
+                
+                </div>
+        </>
+    }
+    
 }
